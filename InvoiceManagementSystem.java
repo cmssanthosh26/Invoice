07abbe424229ed828af2f27c2ec4d4ecd01ce270
cmssanthosh26 <77882030+cmssanthosh26@users.noreply.github.com>
2021-01-29 21:43:38 +0530

    Delete InvoiceManagementSystem.java

--- InvoiceManagementSystem.java
+++
@@ -1,8 +0,0 @@
-package invoice.management.system;
-public class InvoiceManagementSystem {
-    public static void main(String[] args) {
-        InvoiceSystem is=new InvoiceSystem();
-        is.addItemList();
-        is.takeAction();
-    }    
-}
